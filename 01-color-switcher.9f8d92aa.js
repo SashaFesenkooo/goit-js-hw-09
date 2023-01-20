61f6d9f4126a8d7700205536248ac59ee27bc12b
SashaFesenkooo <91009562+SashaFesenkooo@users.noreply.github.com>
2023-01-20 17:44:14 +0000

    Deploying to gh-pages from @ SashaFesenkooo/goit-js-hw-09@9b8c37cd5b2d69c50e04532dbe667457ec97160c 🚀

---
+++ 01-color-switcher.9f8d92aa.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]")};let n;t.btnStop.disabled=!0,t.btnStart.addEventListener("click",(function(){t.btnStart.disabled=!0,t.btnStop.disabled=!1,n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),console.log(n)})),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled=!1,t.btnStop.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.9f8d92aa.js.map
