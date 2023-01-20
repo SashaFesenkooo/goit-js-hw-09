9b8c37cd5b2d69c50e04532dbe667457ec97160c
Sasha Fesenko <sashafesenkooo@gmail.com>
2023-01-20 19:43:51 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]')
+}
 
+let colorId 
+refs.btnStop.disabled = true;
+
+refs.btnStart.addEventListener('click', startBtnClick);
+refs.btnStop.addEventListener('click', stopBtnClick)
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startBtnClick() {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+
+  colorId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  console.log(colorId);
+};
+
+function stopBtnClick() {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+
+  clearInterval(colorId)
+}
